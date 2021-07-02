8d34a915d36c545fc01d9b7e3adb2a8450b2bc49
Anri Liy <amedamachip713@gmail.com>
2021-07-01 23:14:54 -0700

    User In Room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -3,17 +3,45 @@ import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 import Room from './Room';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { Grid, Button, ButtonGroup, Typography} from '@material-ui/core';
 
 export default class HomePage extends Component {
   constructor(props){
     super(props);
   }
 
+  async componentDidMount() {
+    fetch('/api/user-in-room').then((response) => response.json())
+    .then((data) => {});
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   render(){
     return (
       <Router>
         <Switch>
-          <Route exact path='/'><p>This is the Hope Page</p></Route>
+          <Route exact path='/'>{this.renderHomePage()}</Route>
           <Route path='/join' component={RoomJoinPage} />
           <Route path='/create' component={CreateRoomPage} />
           <Route path='/room/:roomCode' component={Room} />
